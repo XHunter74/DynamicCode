30946070c778623239dc7fb4a773955a834c33d9
serhiy.krasovskyy <xhunter74@gmail.com>
2025-05-28 13:10:31 +0300

    Made code more readable.

--- DynamicCompiler.cs
+++ DynamicCompiler.cs
@@ -17,34 +17,34 @@ public class DynamicCompiler
             .Where(a => !a.IsDynamic && !string.IsNullOrEmpty(a.Location))
             .Select(a => MetadataReference.CreateFromFile(a.Location))
             .Cast<MetadataReference>();
-        
+
         var compilation = CSharpCompilation.Create(
             assemblyName: "DynamicAssembly",
             syntaxTrees: [syntaxTree],
             references: references,
             options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
         );
-        
+
         using var ms = new MemoryStream();
-        
+
         var result = compilation.Emit(ms);
-        
+
         if (!result.Success)
         {
             var errors = string.Join(Environment.NewLine, result.Diagnostics.Select(d => d.ToString()));
             throw new Exception("Compilation failed:\n" + errors);
         }
-        
+
         ms.Seek(0, SeekOrigin.Begin);
         var asm = Assembly.Load(ms.ToArray());
         var type = asm.GetType(className);
         var method = type!.GetMethod(methodName, BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance);
-        
+
         if (method == null)
         {
             throw new Exception($"Method '{methodName}' not found in dynamic class.");
         }
-        
+
         return (T)method.CreateDelegate(typeof(T), null);
     }
 
